fbed20632546644f9ef823456fd755ac8dd9ae48
Tianli Ding <td2@illinois.edu>
2020-09-24 03:26:05 -0500

    git

--- src/server.py
+++ src/server.py
@@ -15,7 +15,9 @@ import socket
 
 class Server:
     def __init__(self):
-        self.localIP = "192.168.1.112"
+        # self.localIP = "10.182.138.13"  # 192.168.1.112
+        self.localIP = socket.gethostname()
+        print(self.localIP)
         self.localPort = 8080
         self.bufferSize = 1024
 
