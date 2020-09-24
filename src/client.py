d910925c095ce5085d904a39e4e47b1aef9f521f
Tianli Ding <td2@illinois.edu>
2020-09-24 01:08:08 -0500

    add sockets

--- src/client.py
+++ src/client.py
@@ -5,5 +5,26 @@ for each client:
 - all-to-all: send to all except itself
 """
 
+import socket
 
 
+class Client:
+    def run(self):
+        msgFromClient = "Hello UDP Server"
+        bytesToSend = str.encode(msgFromClient)
+
+        serverAddressPort = ("127.0.0.1", 20001)
+        bufferSize = 1024
+
+        # Create a UDP socket at client side
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        # Send to server using created UDP socket
+        UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+        msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        msg = "Message from Server {}".format(msgFromServer[0])
+        print(msg)
+
+
+if __name__ == '__main__':
+    s = Client()
+    s.run()
--- src/server.py
+++ src/server.py
@@ -4,7 +4,39 @@ global membership list
 id count
 log to record join, leave, fail
 type var: gossip / membership list
+
+VM:
+fa20-cs425-g48-XX.cs.illinois.edu
 """
 
+import socket
 
 
+class Server:
+    def __init__(self):
+        self.localIP = "127.0.0.1"
+        self.localPort = 20001
+        self.bufferSize = 1024
+
+    def run(self):
+        msgFromServer = "Hello UDP Client"
+        bytesToSend = str.encode(msgFromServer)
+        # Create a socket, parameter: Internet, UDP
+        UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+        UDPServerSocket.bind((self.localIP, self.localPort))
+        print("UDP server up and listening")
+
+        while True:
+            message, address = UDPServerSocket.recvfrom(self.bufferSize)
+            clientMsg = "Message from Client:{}".format(message)
+            clientIP = "Client IP Address:{}".format(address)
+            print(clientMsg)
+            print(clientIP)
+            # Sending a reply to client
+            UDPServerSocket.sendto(bytesToSend, address)
+
+
+if __name__ == '__main__':
+    s = Server()
+    s.run()
